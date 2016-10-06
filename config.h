6cda9f8a62cffe4fd814f7c3dabbebd508fc928f
Kanon Kubose <git@herbaloutfitters.com>
2016-10-06 01:07:57 -0700

    Initial commit

---
+++ config.h
@@ -0,0 +1,20 @@
+/* See LICENSE file for copyright and license details. */
+/* Default settings; can be overriden by command line. */
+
+static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int instant = 0;                     /* -n  option; if 1, dmenu ends immediately on a distinct match if typed text is at the beginning */
+static int anyinstant = 0;                     /* -a  option; if 1, dmenu ends immediately on a distinct match even if typed text is not at the beginning */
+static int clearInputField = 0;             /* -c  option; if 1, clear input field upon ctrl-enter (which sends the selection to stdout but doesn't leave the program */
+/* -fn option overrides fonts[0]; default X11 font or font set */
+static const char *fonts[] = {
+	"monospace:size=10"
+};
+static const char *prompt      = NULL;      /* -p  option; prompt to the elft of input field */
+static const char *normbgcolor = "#222222"; /* -nb option; normal background                 */
+static const char *normfgcolor = "#bbbbbb"; /* -nf option; normal foreground                 */
+static const char *selbgcolor  = "#005577"; /* -sb option; selected background               */
+static const char *selfgcolor  = "#eeeeee"; /* -sf option; selected foreground               */
+static const char *outbgcolor  = "#00ffff";
+static const char *outfgcolor  = "#000000";
+/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+static unsigned int lines      = 0;
